10cf7b08cbe2a8e162a0f2c37b856bcf2c2f3cbb
Felipe Lemos <54712007+Felipi12@users.noreply.github.com>
2023-11-23 17:29:34 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,12 @@ bool _isSwitched = false;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DoneApp',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a blue toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
-
-          // useMaterial3: true,
           fontFamily: 'RedHatDisplay',
-          // canvasColor: Color.fromRGBO(1, 169, 94, 1),
           iconTheme: IconThemeData(color: Colors.white)),
       home: const MyHomePage(title: ''),
     );
@@ -46,15 +26,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -79,11 +50,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     super.dispose();
   }
 
-<<<<<<< Updated upstream
   void _handleTabSelection() {
     setState(() {});
-=======
-  // Constrói o layout
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           const Center(child: Text("Compartilhar vem aqui")),
         ],
       ),
-      endDrawer: CustomDrawer(tabController: _tabController),
+      endDrawer: Drawer(tabController: _tabController),
       floatingActionButton:
           _tabController.index == 0 || _tabController.index == 1
               ? FloatingActionButton(
@@ -120,17 +88,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
               : null,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
->>>>>>> Stashed changes
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         bottom: TabBar(
@@ -150,21 +111,12 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             ),
           ],
         ),
-
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Color.fromRGBO(1, 169, 94, 1),
         leading: Image.asset('assets/avatar.png'),
-
-<<<<<<< Updated upstream
         toolbarHeight: 70,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Center(
           child: Text(_getTitleBasedOnTab(_tabController.index)),
         ),
-        //widget.title
         iconTheme: IconThemeData(color: Colors.white, size: 40),
       ),
       body: TabBarView(
@@ -281,8 +233,14 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
               ),
             ),
           ])),
-      // This trailing comma makes auto-formatting nicer for build methods.
-=======
+
+
+
+  // Constrói o Appbar/Navbar
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final TabController tabController;
+  final int selectedIndex;
+
   const CustomAppBar(
       {super.key, required this.tabController, required this.selectedIndex});
 
@@ -299,7 +257,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         child: Text(_getTitleBasedOnTab(selectedIndex)),
       ),
       iconTheme: const IconThemeData(color: Colors.white, size: 40),
->>>>>>> Stashed changes
     );
   }
 
